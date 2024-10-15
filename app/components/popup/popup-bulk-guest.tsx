ae2ae895c2d4f13d1cfea54dc671edb7d15183bb
Muhammad Bafaqih <bafaqih555@gmail.com>
2024-10-15 15:11:26 +0700

    adjust dialog size

--- app/components/popup/popup-bulk-guest.tsx
+++ app/components/popup/popup-bulk-guest.tsx
@@ -48,14 +48,14 @@ export function PopupBulkGuest({
     })
     return (
         <Dialog open={open} onOpenChange={onOpen}>
-            <DialogContent className="sm:max-w-[425px]">
+            <DialogContent className="sm:max-w-[500px]">
                 <DialogHeader>
                     <DialogTitle>Guest Management</DialogTitle>
                     <DialogDescription>
                         Add multiple guest, put multiple guest on new line or press "Enter"
                     </DialogDescription>
                 </DialogHeader>
-                <Textarea ref={textAreaRef} className="w-[250px] max-w-[50vw] h-[500px] max-h-[80vh] p-4 rounded-md focus:outline-none" />
+                <Textarea ref={textAreaRef} className="w-full h-[500px] max-h-[80vh] p-4 rounded-md focus:outline-none" />
                 <DialogFooter className="sm:justify-end">
                     <Button type="submit" onClick={handleSubmit} variant="default">
                         Save
